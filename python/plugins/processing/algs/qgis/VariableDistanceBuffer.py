4c5b3b4e026a39caae2a162c96f17f68f6f01543
Marcus Santos <mvcs@me.com>
2015-06-25 19:54:30 +0100

    - Refactoring: using the new implementation of the Buffer

--- python/plugins/processing/algs/qgis/VariableDistanceBuffer.py
+++ python/plugins/processing/algs/qgis/VariableDistanceBuffer.py
@@ -68,6 +68,6 @@ class VariableDistanceBuffer(GeoAlgorithm):
 
         writer = self.getOutputFromName(self.OUTPUT).getVectorWriter(
             layer.pendingFields().toList(), QGis.WKBPolygon, layer.crs())
-
+        buff = Buffer(parent=self)
         buff.buffering(progress, writer, 0, field, True, layer, dissolve,
                        segments)
