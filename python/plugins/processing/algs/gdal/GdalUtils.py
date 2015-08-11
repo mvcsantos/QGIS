2d70c63b67b2e904716963f74979dd8f7b49826e
Marcus Santos <mvcs@me.com>
2015-08-11 18:26:36 +0100

    - Cancel option on GDAL algorithms

--- python/plugins/processing/algs/gdal/GdalUtils.py
+++ python/plugins/processing/algs/gdal/GdalUtils.py
@@ -28,9 +28,13 @@ __revision__ = '$Format:%H$'
 import os
 import subprocess
 import platform
+import signal
+
+from fcntl import fcntl, F_GETFL, F_SETFL
 from PyQt4.QtCore import QSettings, QObject
 from qgis.core import QgsApplication
 from processing.core.ProcessingLog import ProcessingLog
+from processing.core.CancelledAlgorithmExecutionException import CancelledAlgorithmExecutionException
 
 try:
     from osgeo import gdal
@@ -81,9 +85,32 @@ class GdalUtils(QObject):
             stdin=open(os.devnull),
             stderr=subprocess.STDOUT,
             universal_newlines=True,
+            preexec_fn=os.setsid
         ).stdout
         self.parent().setInfo.emit('GDAL command output:')
-        for line in iter(proc.readline, ''):
+        
+        # set the O_NONBLOCK flag of proc.stdout file descriptor:
+        flags = fcntl(proc.stdout, F_GETFL) # get current proc.stdout flags
+        fcntl(proc.stdout, F_SETFL, flags | os.O_NONBLOCK)
+        
+        output = None
+        
+        while True:
+            try:
+                output = os.read(proc.stdout.fileno(), 1024),
+                break
+            except OSError:
+                # the os throws an exception if there is no data
+                if self.parent().executionCancelled:
+                    self.parent().executionCancelled = False
+                    os.killpg(proc.pid, signal.SIGTERM)
+                    self.parent().setInfo.emit('GRASS execution cancelled')
+                    raise CancelledAlgorithmExecutionException()
+                
+        
+        output = output[0].splitlines()
+        
+        for line in output:
             self.parent().setConsoleInfo.emit(line)
             loglines.append(line)
         ProcessingLog.addToLog(ProcessingLog.LOG_INFO, loglines)
